9a37d830a6f205fd77d5e847c2e0b9c0480d123e
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-22 20:00:04 -0300

    chore: update shareCode to RA4C3KNXG2Y

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=BXCGWK8WMOI&afp=tapib' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=RA4C3KNXG2Y&afp=tapib' });
     res.end();
   }
