8386da11a15a035c899612e4bebfbe5df8415d90
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-25 20:38:22 -0300

    chore: update shareCode to DLCWVBULSCJ

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=Y1WT1QCRWAD&afp=tapib' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=DLCWVBULSCJ&afp=tapib' });
     res.end();
   }
