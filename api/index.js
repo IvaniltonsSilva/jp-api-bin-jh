c108713e545063432f72f6ccd90537351d8cc54e
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-26 19:17:01 -0300

    chore: update shareCode to NAUGU76T3VC

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=DLCWVBULSCJ&afp=tapib' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/8l8xs4rb?shareCode=NAUGU76T3VC&afp=tapib' });
     res.end();
   }
